f42395e382aa130b75b8f66dce11f30d63dca31a
rodrigospm <rodrigo.perez@ucsp.edu.pe>
2022-10-19 18:14:42 -0500

    Sfml pantalla fix

--- Makefile
+++ Makefile
@@ -7,4 +7,5 @@ link:
 	g++ main.o -o main -L"C:\Stuff\libraries\SFML-2.5.1\lib" -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32 -mwindows -lsfml-main
 
 clean:
-	rd -f main.o 
+	del main.o
+	del main.exe
--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+/* #include <iostream>
 #include <array>
 #include "personaje.h"
 #include "mapa.h"
-using namespace std;
-
+using namespace std; */
 
 int main()
 {   
-    // Creacion de personajes
+
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+}
+}
+
+
+
+
+    // Creacion de personajes 
+    /*
     int atja[3]={1200000,310,4100};
     int salu[4]={120,340,340, 10000};
     Personaje j("jorge",'M',atja,salu);
@@ -23,7 +47,8 @@ int main()
     int m1[2]{1080, 720};
     Mapa mapa1(m1);
     return 0;
-}
+    */
+
 
 /* #include <SFML/Graphics.hpp>
 
