05d63b7f40e14d97824e8a0b5893b1f51f33d79a
jlennon02 <112089009+jlennon02@users.noreply.github.com>
2022-11-23 14:11:34 -0500

    Update Personaje.h
    falto por elegir forma

--- Class/Personaje.h
+++ Class/Personaje.h
@@ -2,20 +2,71 @@
 #define _PERSONAJE_H_
 #include <string>
 #include "Entidad.h"
+#include "Salud.h" //   por si elijen la 1
 
 
 class Personaje : public Entidad{
     public:
-        Personaje(std::string id, std::string nombre, int edad);
-        Personaje();
-        Personaje(Personaje &o);
-
-        void setNombre(std::string);
-        std::string getNombre() const;
-        void setEdad(int);
-        int getEdad() const;
+        Personaje(std::string _id, int _atr_inteligencia, Salud &o[3]);///   1
+        // elijan la formas correcta para ustedes 1 o 2
+        //Personaje(std::string _id, int _inteligencia, int _hambre, int _sed, int _stamina);///    2
+    
+        ///Personaje(Personaje &o);
+    
+        void getid(std::string _id){
+            id=_id;
+        }
+        std::string setid() const{
+            return id;
+        }
+        
+        void getinteligencia(int _atr_inteligencia){
+            inteligencia=_atr_inteligencia;
+        }
+        int setinteligencia() const{
+            return inteligencia;
+        }
+        /*
+        void getsalud(Salud &o[3]){
+            fuerza=o[0];
+            agilidad=o[1];
+            inteligencia=o[2];
+        }
+        
+        Salud setfuerza() const{
+            ////// sobrecarga de operador
+        }
+        */
+    
+    
+    /*
+        void gethambre(int _hambre){
+            hambre=_hambre;
+        }
+        int sethambre() const{
+            return hambre;
+        }
+    
+        void getsed(int _sed){
+            sed=_sed;
+        }
+        int setsed() const{
+            return sed
+        }
+    
+        void getstamina(int _stamina){
+            stamina=_stamina;
+        }
+        int setstamina() const{
+            return stamina
+        }
+    */
     private:
-        std::string nombre;
-        int edad;        
+        std::string id;
+        int inteligencia;
+        Salud bar_estados[3];
+        // int hambre=100;
+        // int sed=100;
+        // int stamina=100;
 };
 #endif
