e221efc4e60e3e09f22bc2897157f71f62a816d1
jlennon02 <112089009+jlennon02@users.noreply.github.com>
2022-11-23 11:46:43 -0500

    Update Animal.h
    aun falta insertar un sprite

--- Class/Animal.h
+++ Class/Animal.h
@@ -1,16 +1,41 @@
 #ifndef _ANIMAL_H_
 #define _ANIMAL_H_
 #include <string>
+#include "entidad.h"
+#include "Personaje.h"
+#include <SFML/Graphics.hpp> 
 
 class Animal : public Entidad {
     public:
-        Animal(std::string id;std::strin nombre, int edad);
-        Animal();
-        Animal(Personaje &o);
+        Animal(std::string nombre):edad(edad), nombre(nombre){};
+        Animal(sf::Texture &texture):texture(texture) {};
+        
+        void setnombrel(std::string nombre) {
+            nombre = nombre;
+        }
+        std::string getnombre() const {
+            return nombre;
+        }
 
-        void setAnimal(std::string);
-        std::string getAnimal() const;
+
+        void setedad(int edad){
+            edad = edad;
+        }
+        int getedad() const{
+            return edad;
+        }
+
+        void settexture(sf::Texture o) {
+            texture = o;
+        }
+        void mostrar_sprite() {
+            // codigo de impresion de sprite 
+        }
+
+
     private:
-        std::string animal;        
+        sf::Texture texture;
+        std::string nombre;
+        int edad;
 };
 #endif
