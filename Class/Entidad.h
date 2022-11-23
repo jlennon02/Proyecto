8683ef58d4c93ffd532b53ffa5c5ae9ef6b4086b
jlennon02 <112089009+jlennon02@users.noreply.github.com>
2022-11-23 12:24:04 -0500

    Update Entidad.h
    Aun faltan agregar metodos a la clase, si lo hace alguien mas recuerde que  algunos metodos son virtuales por el polimorfismo

--- Class/Entidad.h
+++ Class/Entidad.h
@@ -4,16 +4,40 @@
 
 class Entidad {
     public:
-        Entidad() {}
-
-        void setId(std::string go){}
-
-        void setVida(bool);
-        int getVida() const;
-
+        Entidad(std::string _nombre, int _hp, int _atr_agilididad, int _atr_fuerza): nombre(_nombre), hp(_hp), agilidad(_atr_agilidad), fuerza(_atr_fuerza){}
+        ////    variables miembro son nombre ,hp ,agilidad ,fuerza
+        ////    estas se heredan a las subclases
+        void getnombre(std::string _nombre){
+            nombre=_nombre;
+        }
+        std::string setnombre() const{
+            return nombre;
+        }
+    
+        void gethp(int _hp){
+            hp=_hp;
+        }
+        int sethp() const{
+            return hp;
+        }
+    
+        void getagilidad(int _atr_agilidad){
+            agilidad=_atr_agilidad;
+        }
+        int setagilidad() const{
+            return agilidad;
+        }
+    
+        void getfuerza(int _atr_fuerza){
+            fuerza=_atr_fuerza;
+        }
+        int setfuerza() const{
+            return fuerza;
+        }
     protected:
-        std::string id;
-        bool vida;
-
+        std::string nombre;
+        int hp;
+        int agilidad;
+        int fuerza;
 };
 #endif
