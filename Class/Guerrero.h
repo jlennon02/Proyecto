4f275aac96ffd569b425a757a7df1b1b4ad133ae
jlennon02 <112089009+jlennon02@users.noreply.github.com>
2022-11-24 05:51:22 -0500

    Update Guerrero.h

--- Class/Guerrero.h
+++ Class/Guerrero.h
@@ -1,1 +1,33 @@
 
+#ifndef _GUERRERO_H_
+#define _GUERRERO_H_
+#include "Personaje.h"
+
+class Guerrero: public Personaje{
+  public:
+    Archer(int _daño, int _hitbox, int _vel_ataq):daño(_daño), hitbox(_hitbox), vel_ataq(_vel_ataq){}
+    void getdaño(int _daño){
+      daño = _daño;
+    }
+    int setadaño() const{
+      return daño;
+    }
+      
+    void gethitbox(int _hitbox){
+       hitbox = _hitbox;
+    }
+    int sethitbox() const{
+      return hitbox;
+    }
+      
+    void gethitbox(int _vel_ataq){
+       vel_ataq = vel_ataq;
+    }
+    int
+  
+  private:
+    int daño;
+    int hitbox;
+    int vel_ataq;
+}
+#endif
